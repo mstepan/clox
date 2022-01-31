dfcceda462b1b1e9e39af220143fc14174fb3c80
Maksym Stepanenko <maksym.stepanenko@opower.com>
2022-01-31 12:05:56 +0200

    Chunk files added.

---
+++ src/chunk.c
@@ -0,0 +1,7 @@
+#include "chunk.h"
+#include <stdio.h>
+
+int printMe(){
+	printf("Print me called\n");
+	return 0;
+}
--- src/main.c
+++ src/main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+#include "chunk.h"
 
 int main(int argc, const char* argv[]){
+
+	printMe();
+
 	printf("clox main completed\n");
+
 	return 0;
 }
