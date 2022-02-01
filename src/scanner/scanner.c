f59f1751737f43788160f7e87dea3c2eb0f7c185
Maksym Stepanenko <maksym.stepanenko@opower.com>
2022-02-01 18:25:23 +0200

    Basic scanner/lexer added.

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -1,4 +1,7 @@
 #include <stdio.h>
+#include <string.h>
+#include "../util/common.h"
+#include "scanner.h"
 
 typedef struct {
 
@@ -21,5 +24,121 @@ void initScanner(const char *source) {
     scanner.current = source;
     scanner.line = 1;
     printf("Scanner initialized successfully\n");
+}
+
+static bool isAtEnd() {
+    return *scanner.current == '\0';
+}
+
+static Token makeToken(TokenType type) {
+    Token token;
+    token.type = type;
+    token.start = scanner.start;
+    token.length = (int) (scanner.current - scanner.start);
+    token.line = scanner.line;
+
+    return token;
+}
+
+static Token errorToken(const char *errorMsg) {
+    Token token;
+    token.type = TOKEN_ERROR;
+    token.start = errorMsg;
+    token.length = (int) strlen(errorMsg);
+    token.line = scanner.line;
+
+    return token;
+}
+
+static char advance() {
+    char ch = *scanner.current;
+    scanner.current++;
+    return ch;
+}
+
+static bool match(char expectedCh) {
+    if (isAtEnd()) {
+        return false;
+    }
+    if (*scanner.current != expectedCh) {
+        return false;
+    }
+    scanner.current++;
+    return true;
+}
+
+static char peek() {
+    return *scanner.current;
+}
+
+/*
+ * Skip all whitespace character + skip single line comments.
+ */
+static void skipWhitespaces() {
+    for (;;) {
+        char c = peek();
+        switch (c) {
+            case ' ':
+            case '\r':
+            case '\t':
+                advance();
+                break;
+            case '\n':
+                scanner.line++;
+                advance();
+                break;
+            default:
+                return;
+        }
+    }
+}
+
+Token scanToken() {
+    skipWhitespaces();
+    scanner.start = scanner.current;
+
+    if (isAtEnd()) {
+        return makeToken(TOKEN_EOF);
+    }
+
+    char c = advance();
+
+    switch (c) {
+        // 1 char match
+        case '(':
+            return makeToken(TOKEN_LEFT_PAREN);
+        case ')':
+            return makeToken(TOKEN_RIGHT_PAREN);
+        case '{':
+            return makeToken(TOKEN_LEFT_BRACE);
+        case '}':
+            return makeToken(TOKEN_RIGHT_BRACE);
+        case ',':
+            return makeToken(TOKEN_COMMA);
+        case '.':
+            return makeToken(TOKEN_DOT);
+        case ';':
+            return makeToken(TOKEN_SEMICOLON);
+        case '+':
+            return makeToken(TOKEN_PLUS);
+        case '-':
+            return makeToken(TOKEN_MINUS);
+        case '*':
+            return makeToken(TOKEN_STAR);
+        case '/':
+            return makeToken(TOKEN_SLASH);
+
+            // 1 or 2 chars match
+        case '!':
+            return makeToken(match('=' ? TOKEN_BANG_EQUAL : TOKEN_EQUAL_EQUAL));
+        case '=':
+            return makeToken(match('=' ? TOKEN_EQUAL_EQUAL : TOKEN_EQUAL));
+        case '<':
+            return makeToken(match('=' ? TOKEN_LESS_EQUAL : TOKEN_LESS));
+        case '>':
+            return makeToken(match('=' ? TOKEN_GREATER_EQUAL : TOKEN_GREATER));
+    }
+
+    return errorToken("Unexpected character");
 }
 
--- src/scanner/scanner.h
+++ src/scanner/scanner.h
@@ -1,8 +1,65 @@
 #ifndef CLOX_SCANNER_H
 #define CLOX_SCANNER_H
 
+typedef enum {
+
+    // 1 char
+    TOKEN_LEFT_PAREN, // (
+    TOKEN_RIGHT_PAREN, // )
+    TOKEN_LEFT_BRACE, // {
+    TOKEN_RIGHT_BRACE, // }
+    TOKEN_COMMA, // ,
+    TOKEN_DOT, // .
+    TOKEN_SEMICOLON, // ;
+    TOKEN_MINUS, // -
+    TOKEN_PLUS, // +
+    TOKEN_STAR, // *
+    TOKEN_SLASH, // /
+
+    // 1 or 2 chars
+    TOKEN_BANG, // !
+    TOKEN_BANG_EQUAL, // !=
+    TOKEN_EQUAL, // =
+    TOKEN_EQUAL_EQUAL, // ==
+    TOKEN_GREATER, // >
+    TOKEN_GREATER_EQUAL, // >=
+    TOKEN_LESS, // <
+    TOKEN_LESS_EQUAL, // <=
+
+    // literals
+    TOKEN_IDENTIFIER, TOKEN_STRING, TOKEN_NUMBER,
+
+    // reserved keywords
+    TOKEN_AND, TOKEN_OR,
+    TOKEN_CLASS, TOKEN_SUPER, TOKEN_THIS,
+    TOKEN_IF, TOKEN_ELSE,
+    TOKEN_WHILE, TOKEN_FOR,
+    TOKEN_TRUE, TOKEN_FALSE,
+    TOKEN_NIL,
+    TOKEN_PRINT,
+    TOKEN_RETURN,
+    TOKEN_VAR, TOKEN_FUN,
+
+    TOKEN_ERROR,
+    TOKEN_EOF,
+
+}TokenType;
+
+typedef struct {
+    TokenType type;
+
+    // pointer to token start
+    const char* start;
+
+    // token length
+    int length;
+
+    // line number
+    int line;
+} Token;
+
 void initScanner(const char *source);
 
-
+Token scanToken();
 
 #endif //CLOX_SCANNER_H
