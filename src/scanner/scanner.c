f66f15cc60c9c8750ff36a4a0f96114a4ddd8752
Maksym Stepanenko <maksym.stepanenko@opower.com>
2022-02-02 11:33:41 +0200

    Scanner fully completed.

--- src/scanner/scanner.c
+++ src/scanner/scanner.c
@@ -54,6 +54,8 @@ static Token identifier();
 
 static TokenType identifierType();
 
+static TokenType checkKeyword(int start, int length, const char *rest, TokenType type);
+
 Token scanToken() {
     skipWhitespaces();
     scanner.start = scanner.current;
@@ -196,6 +198,16 @@ static void skipWhitespaces() {
     }
 }
 
+static bool isDigit(char ch) {
+    return ch >= '0' && ch <= '9';
+}
+
+static bool isAlpha(char ch) {
+    return (ch >= 'a' && ch <= 'z') ||
+           (ch >= 'A' && ch <= 'Z') ||
+           ch == '_';
+}
+
 static Token string() {
     while (peek() != '"' && !isAtEnd()) {
         // we support multi-line strings, so just handle new line properly
@@ -210,9 +222,6 @@ static Token string() {
     return makeToken(TOKEN_STRING);
 }
 
-static bool isDigit(char ch) {
-    return ch >= '0' && ch <= '9';
-}
 
 static Token number() {
     while (isDigit(peek())) advance();
@@ -228,10 +237,12 @@ static Token number() {
     return makeToken(TOKEN_NUMBER);
 }
 
-static bool isAlpha(char ch) {
-    return (ch >= 'a' && ch <= 'z') ||
-           (ch >= 'A' && ch <= 'Z') ||
-           ch == '_';
+
+static Token identifier() {
+    // consume all alphabets or digits
+    while (isAlpha(peek()) || isDigit(peek())) advance();
+
+    return makeToken(identifierType());
 }
 
 /*
@@ -239,15 +250,65 @@ static bool isAlpha(char ch) {
  */
 static TokenType identifierType() {
 
+    // build Trie like DS, but only using simple switch statements.
+    switch (scanner.start[0]) {
+        case 'a':
+            return checkKeyword(1, 2, "nd", TOKEN_AND);
+        case 'c':
+            return checkKeyword(1, 4, "lass", TOKEN_CLASS);
+        case 'e':
+            return checkKeyword(1, 3, "lse", TOKEN_ELSE);
+        case 'f': {
+            if (scanner.current - scanner.start > 1) {
+                switch (scanner.start[1]) {
+                    case 'a':
+                        return checkKeyword(2, 3, "lse", TOKEN_FALSE);
+                    case 'o':
+                        return checkKeyword(2, 1, "r", TOKEN_FOR);
+                    case 'u':
+                        return checkKeyword(2, 1, "n", TOKEN_FUN);
+                }
+            }
+            break;
+        }
+        case 'i':
+            return checkKeyword(1, 1, "f", TOKEN_IF);
+        case 'n':
+            return checkKeyword(1, 2, "il", TOKEN_NIL);
+        case 'o':
+            return checkKeyword(1, 1, "r", TOKEN_OR);
+        case 'p':
+            return checkKeyword(1, 4, "rint", TOKEN_PRINT);
+        case 'r':
+            return checkKeyword(1, 5, "eturn", TOKEN_RETURN);
+        case 's':
+            return checkKeyword(1, 4, "uper", TOKEN_SUPER);
+        case 't': {
+            if (scanner.current - scanner.start > 1) {
+                switch (scanner.start[1]) {
+                    case 'h':
+                        return checkKeyword(2, 2, "is", TOKEN_THIS);
+                    case 'r':
+                        return checkKeyword(2, 2, "ue", TOKEN_TRUE);
+                }
+                break;
+            }
+            case 'v':
+                return checkKeyword(1, 2, "ar", TOKEN_VAR);
+            case 'w':
+                return checkKeyword(1, 4, "hile", TOKEN_WHILE);
+        }
+    }
 
-    //TODO:
     return TOKEN_IDENTIFIER;
 }
 
-static Token identifier() {
-    // consume all alphabets or digits
-    while (isAlpha(peek()) || isDigit(peek())) advance();
 
-    return makeToken(identifierType());
+static TokenType checkKeyword(int start, int length, const char *rest, TokenType type) {
+    if (scanner.current - scanner.start == start + length && memcmp(scanner.start + start, rest, length) == 0) {
+        return type;
+    }
+
+    return TOKEN_IDENTIFIER;
 }
 
